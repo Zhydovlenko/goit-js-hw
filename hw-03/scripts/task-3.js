f6e47e5b0537ebbe17661c4772fbeee9e3145c40
Zhydovlenko <you@example.com>
2020-02-09 21:43:18 +0200

    module-03

---
+++ hw-03/scripts/task-3.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const findBestEmployee = function(employees) {
+  let max = 0;
+  let bestEmployee;
+
+  const keys = Object.keys(employees);
+
+  for (const key of keys) {
+    if (employees[key] > max) {
+      max = employees[key];
+      bestEmployee = key;
+    }
+  }
+
+  return bestEmployee;
+};
+
+/*
+ if (numOfTasks > max) {
+            max = numOfTasks;
+            bestEmployee = employee;
+        }
+    }
+
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }),
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }),
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }),
+); // lux
